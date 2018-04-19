23a07770eedb184764bdb7466023cc5f6e22f2df
hotuta <hotuta@users.noreply.github.com>
2018-04-19 10:53:19 +0900

    Chromeのバイナリ指定を追加

--- config/initializers/capybara.rb
+++ config/initializers/capybara.rb
@@ -1,6 +1,10 @@
 Rails.application.config.to_prepare do
+  chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
+  chrome_opts = chrome_bin ? {chromeOptions: {binary: chrome_bin}} : {}
+
+  caps = Selenium::WebDriver::Remote::Capabilities.chrome(chrome_opts)
   Capybara.register_driver :selenium do |app|
-    Capybara::Selenium::Driver.new(app, browser: :chrome)
+    Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: caps)
   end
 
   Capybara.configure do |config|
