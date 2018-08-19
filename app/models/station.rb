b9200d8ef49d22f962d6ad08dcb704669ecbf0d1
hotuta <hotuta@users.noreply.github.com>
2018-08-19 10:59:59 +0900

    レイヤー削除判定回数を5回に変更

--- app/models/station.rb
+++ app/models/station.rb
@@ -189,7 +189,7 @@ class Station < ApplicationRecord
     end
 
     def delete_layer_has_xpath
-      loop do
+      5.times do
         @session.refresh
         sleep 15
         if @session.has_xpath?(@delete_has_xpath)
