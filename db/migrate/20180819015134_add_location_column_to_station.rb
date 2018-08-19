84b1e46d58496138af80f05cb7ef93428b8b9743
hotuta <hotuta@users.noreply.github.com>
2018-08-19 11:27:43 +0900

    ポートの座標と説明のカラムを追加

---
+++ db/migrate/20180819015134_add_location_column_to_station.rb
@@ -0,0 +1,9 @@
+class AddLocationColumnToStation < ActiveRecord::Migration[5.1]
+  def change
+    change_table :stations do |t|
+      t.decimal :latitude,  precision: 11, scale: 8
+      t.decimal :longitude, precision: 11, scale: 8
+      t.text :description
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180625110006) do
+ActiveRecord::Schema.define(version: 20180819015134) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,9 @@ ActiveRecord::Schema.define(version: 20180625110006) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "park_id"
+    t.decimal "latitude", precision: 11, scale: 8
+    t.decimal "longitude", precision: 11, scale: 8
+    t.text "description"
     t.index ["numbering"], name: "index_stations_on_numbering", unique: true
   end
 
